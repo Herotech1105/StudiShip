d6b1a54a1dd3f00736566346b1568dd2b052cb33
Lennart Esch <lennart.esch@innosystec.de>
2026-02-12 15:36:49 +0100

    Backend: Now hands room information to hbs file

--- model/roomManager.js
+++ model/roomManager.js
@@ -11,10 +11,16 @@ function dashboardWithRoomList(req, res, db) {
 
 function room(req, res, db) {
     const roomId = req.query.roomId
-    db.query('SELECT * FROM rooms WHERE rooms.id = ? ', [roomId], async (error, result) => {
-        console.log(result)
-        return res.render('room', {
-            room: result
+    db.query('SELECT * FROM rooms WHERE rooms.id = ? ', [roomId], async (error, room) => {
+        db.query('SELECT users.name as user, room_messages.content, room_messages.timestamp FROM (SELECT * FROM messages WHERE messages.room_id = ?) as room_messages LEFT JOIN users ON room_messages.user_id = users.id ? ', [roomId], async (error, messages) => {
+            db.query('SELECT users.name FROM (SELECT user_id FROM roommembers WHERE room_id = ?) ids left join users ON ids.user_id = users.id', [roomId], async (error, members) => {
+                console.log(room[0], messages, members)
+                return res.render('room', {
+                    room: room[0],
+                    messages: messages,
+                    members: members
+                })
+            })
         })
     })
 }
