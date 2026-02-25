cf320857424ea494d71a906f8a4bd0bf8f7420da
Lennart Esch <lennart.esch@innosystec.de>
2026-02-25 11:55:37 +0100

    Backend: now renders subject list in searchResult

--- model/searchManager.js
+++ model/searchManager.js
@@ -1,8 +1,9 @@
 module.exports = {findRooms}
 
-function findRooms(req, res, db) {
+async function findRooms(req, res, db) {
     const {roomName, roomSubject} = req.body
     const user = req.signedCookies['user']
+    const subjects = await require("./subjects")()
     db.query('SELECT users.id ' +
         'FROM users ' +
         'WHERE users.name = ?', [user], async (error, userId) => {
@@ -10,24 +11,28 @@ function findRooms(req, res, db) {
             db.query("SELECT * FROM rooms LEFT JOIN (SELECT roommembers.room_id FROM roommembers WHERE roommembers.user_id = ?) member ON member.room_id = rooms.id WHERE member.room_id IS NULL AND rooms.privacy = 'public' AND rooms.name LIKE ? AND rooms.subject = ?", [userId[0].id, "%" + roomName + "%", roomSubject], async (error, rooms) => {
                 res.render('searchResult', {
                     rooms: rooms,
+                    subjects: subjects,
                 })
             })
         } else if (roomSubject !== "any") {
             db.query("SELECT * FROM rooms LEFT JOIN (SELECT roommembers.room_id FROM roommembers WHERE roommembers.user_id = ?) member ON member.room_id = rooms.id WHERE member.room_id IS NULL AND rooms.privacy = 'public' AND rooms.subject = ?" , [userId[0].id, roomSubject], async (error, rooms) => {
                 res.render('searchResult', {
                     rooms: rooms,
+                    subjects: subjects,
                 })
             })
         } else if (roomName) {
             db.query("SELECT * FROM rooms LEFT JOIN (SELECT roommembers.room_id FROM roommembers WHERE roommembers.user_id = ?) member ON member.room_id = rooms.id WHERE member.room_id IS NULL AND rooms.privacy = 'public' AND rooms.name LIKE ?", [userId[0].id, "%" + roomName + "%"], async (error, rooms) => {
                 res.render('searchResult', {
                     rooms: rooms,
+                    subjects: subjects,
                 })
             })
         } else {
             db.query("SELECT * FROM rooms LEFT JOIN (SELECT roommembers.room_id FROM roommembers WHERE roommembers.user_id = ?) member ON member.room_id = rooms.id WHERE member.room_id IS NULL AND rooms.privacy = 'public'", [userId[0].id], async (error, rooms) => {
                 res.render('searchResult', {
                     rooms: rooms,
+                    subjects: subjects,
                 })
             })
         }
