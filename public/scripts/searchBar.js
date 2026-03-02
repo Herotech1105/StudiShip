36c0cb50aceb0c32e7e0f9e0547da35abd1b738d
BarnyCoding <steiner.barnab-it25@it.dhbw-ravensburg.de>
2026-03-02 10:45:22 +0100

    Html: dashboard.hbs suche button connected with backend

--- public/scripts/searchBar.js
+++ public/scripts/searchBar.js
@@ -1,17 +1,26 @@
 const searchBar = document.getElementById("searchBar");
+const submitButton = document.getElementById("submitButton");
 searchBar.addEventListener("keydown", (e) => {
     if (e.keyCode === 13) {
-        const query = location.origin + "/search";
-        const form = document.createElement("form");
-        form.setAttribute("action", query);
-        form.setAttribute("method", "POST");
-
-        const subject = document.createElement("input");
-        subject.name = "roomSubject";
-        subject.value = "any";
-        form.appendChild(searchBar);
-        form.appendChild(subject);
-        document.body.appendChild(form);
-        form.submit();
+        submitForm()
     }
 })
+
+submitButton.addEventListener("click", (e) => {
+    submitForm()
+})
+
+function submitForm() {
+    const query = location.origin + "/search";
+    const form = document.createElement("form");
+    form.setAttribute("action", query);
+    form.setAttribute("method", "POST");
+
+    const subject = document.createElement("input");
+    subject.name = "roomSubject";
+    subject.value = "any";
+    form.appendChild(searchBar);
+    form.appendChild(subject);
+    document.body.appendChild(form);
+    form.submit();
+}
